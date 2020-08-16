c9fe3317df7f7bca9d1a42ecf1b40e85bcfaf266
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-08-16 21:33:40 +0800

    Update Macaddress_Changer.py

--- MAC_Address_Changer/Macaddress_Changer.py
+++ MAC_Address_Changer/Macaddress_Changer.py
@@ -4,18 +4,20 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
+def get_arguments():  #This is to get the argument from the user
+    parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest= "interface", help="Interface to change MAC Address")
-parser.add_option("-m", "--mac", dest="mac_add", help="Specify the new mac address")
+    parser.add_option("-i", "--interface", dest= "interface", help="Interface to change MAC Address")
+    parser.add_option("-m", "--mac", dest="mac_add", help="Specify the new mac address")
 
-(options, arguments) = parser.parse_args()
-
-# subprocess.call("sudo ifconfig", shell=True)
-interface = options.interface
-mac_add = options.mac_add
+    return parser.parse_args()
 
 
-subprocess.call(["sudo", "ifconfig", interface, "down"])
-subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac_add])
-subprocess.call(["sudo", "ifconfig", interface, "up"])
+def change_mac(interface, mac_add):  #This will be used to change the MAC Address
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac_add])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+
+(options, argument) = get_arguments()
+change_mac(options.interface, options.mac_add )#Call a function is basically the same as using, be sure to call the the variables
