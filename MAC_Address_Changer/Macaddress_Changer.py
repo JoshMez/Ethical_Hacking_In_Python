df6734d8eff589f406cde464fbdaf48be7f1df59
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-08-16 22:06:59 +0800

    Update Macaddress_Changer.py

--- MAC_Address_Changer/Macaddress_Changer.py
+++ MAC_Address_Changer/Macaddress_Changer.py
@@ -9,8 +9,14 @@ def get_arguments():  #This is to get the argument from the user
 
     parser.add_option("-i", "--interface", dest= "interface", help="Interface to change MAC Address")
     parser.add_option("-m", "--mac", dest="mac_add", help="Specify the new mac address")
+    (options, argument) =  parser.parse_args()
+    if not options.interface :
+        #code to handle error
+        parser.error("[-] Please enter a valid interface ")
+    elif not options.mac_add :
+        #code to handle error
+        parser.error("[-] Please enter a valid mac address ")
 
-    return parser.parse_args()
 
 
 def change_mac(interface, mac_add):  #This will be used to change the MAC Address
@@ -19,5 +25,5 @@ def change_mac(interface, mac_add):  #This will be used to change the MAC Addres
     subprocess.call(["sudo", "ifconfig", interface, "up"])
 
 
-(options, argument) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.mac_add )#Call a function is basically the same as using, be sure to call the the variables
